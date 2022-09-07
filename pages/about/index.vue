bff2424d6c1d5363ce65566044d6567a1e395302
niccybee <niccybee@users.noreply.github.com>
2022-09-08 08:41:40 +1000

    add cv pdf

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,15 +1,12 @@
 <template>
   <main class="home">
-    <Hero 
-      :title="HeroContents.title" 
-      :copy="HeroContents.copy" 
-      :cta_link="HeroContents.cta_link" 
-      :cta_text="HeroContents.cta_text" />
-    <TextSection
-      id="about-me"
-      :text="textSect.header"
-      :header="textSect.text"
-     />
+    <Hero
+      :title="HeroContents.title"
+      :copy="HeroContents.copy"
+      :cta_link="HeroContents.cta_link"
+      :cta_text="HeroContents.cta_text"
+    />
+    <!-- <TextSection id="about-me" :text="textSect.header" :header="textSect.text" /> -->
   </main>
 </template>
 
@@ -19,29 +16,29 @@ import TextSection from "@/components/structure/main/TextSection";
 export default {
   head() {
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+      script: [{ src: "https://identity.netlify.com/v1/netlify-identity-widget.js" }],
     };
   },
   components: {
     Hero,
-    TextSection
+    TextSection,
   },
-  data () {
+  data() {
     return {
       HeroContents: {
         title: "About Me",
-        copy: "First my mum, met my dad. Hi I'm Nic, and my life... it's kinda crazy.",
+        copy:
+          "Digital Marketing Manager, with 5+ years experience in delivering high-converting lead-generation and sales campaigns. Passion for learning new technologies and creating strategies for business success.",
         cta_link: "#about-me",
-        cta_text: "All about me"
+        cta_text: "CV",
       },
       textSect: {
-        header: 'This is the stuff about me!',
-        text: 'Not too much to know really...'
-      }
-    }
-  }
+        header: "This is the stuff about me!",
+        text: "CV",
+      },
+    };
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
