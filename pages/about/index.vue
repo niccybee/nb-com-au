bb118384205f545fcf3b99313694ec84306f0709
niccybee <niccybee@users.noreply.github.com>
2022-09-13 14:19:28 +1000

    sync changes

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -28,7 +28,8 @@ export default {
         title: "About Me",
         copy:
           "Digital Marketing Manager, with 5+ years experience in delivering high-converting lead-generation and sales campaigns. Passion for learning new technologies and creating strategies for business success.",
-        cta_link: "~/assets/CV-Sept-22.pdf",
+        cta_link:
+          "https://github.com/niccybee/nb-com-au/blob/50e0f9f41de8c18ebb26ee91d2bd485930f4f1ac/assets/CV-Sept-22.pdf",
         cta_text: "CV",
       },
       textSect: {
