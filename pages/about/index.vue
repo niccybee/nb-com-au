ebe685d8c7fc753c054f0b2052a615bd808434fa
niccybee <niccybee@users.noreply.github.com>
2022-09-21 00:54:18 +1000

    text colour with p

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -27,7 +27,7 @@ export default {
       HeroContents: {
         title: "About Me",
         copy:
-          "Digital Marketing Manager, with 5+ years experience in delivering high-converting lead-generation and sales campaigns. Passion for learning new technologies and creating strategies for business success.",
+          "<p>Digital Marketing Manager, with 5+ years experience in delivering high-converting lead-generation and sales campaigns. Passion for learning new technologies and creating strategies for business success.</p>",
         cta_link:
           "https://github.com/niccybee/nb-com-au/blob/50e0f9f41de8c18ebb26ee91d2bd485930f4f1ac/assets/CV-Sept-22.pdf",
         cta_text: "CV",
